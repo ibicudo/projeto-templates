6b653bb8ed06080c28b1c334dd27fde8248f3746
ibicudo <ingrid.bicudo@mercadolivre.com>
2021-10-15 10:33:20 -0300

    adding project

---
+++ src/main/java/com/example/projectVersion8/projectVersion8/ProjectVersion8Application.java
@@ -0,0 +1,14 @@
+package com.example.projectVersion8.projectVersion8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectVersion8Application {
+
+	public static void main(String[] args) {
+		System.out.println("Olá mundo");
+		SpringApplication.run(ProjectVersion8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectVersion8/projectVersion8/ProjectVersion8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectVersion8.projectVersion8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectVersion8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
